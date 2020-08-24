85756c0271ba98f9bc36a6a4d26f5d0824de22eb
Aziz Gasimov <aziz.gasimov94@gmail.com>
2020-08-24 20:02:59 +0200

    Login functionality implemented

--- src/service/menu/MenuLoginService.java
+++ src/service/menu/MenuLoginService.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service.menu;
 
-/**
- *
- * @author azizg
- */
-public class MenuLoginService {
-    
+import java.util.Scanner;
+import service.menu.inter.MenuLoginServiceInter;
+
+public class MenuLoginService implements MenuLoginServiceInter {
+
+    private int count = 0;
+
+    @Override
+    public void process() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter username:");
+        String username = sc.nextLine();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("enter password:");
+        String password = sc2.nextLine();
+
+        while (!(username.equals("aziz") && password.equals("12345"))) {
+            count++;
+            if (count == 3) {
+                throw new IllegalArgumentException("user name or password is invalid!");
+            } else {
+                process();
+            }
+        }
+    }
+
 }
